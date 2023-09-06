5dfb9b92020369caeb8ce14ea360d3c8f27c71a8
Siyuexi <siyuexi@foxmail.com>
2023-09-06 17:03:06 +0800

    --Add=Re-direct output to shell.log

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 python -m torch.distributed.launch \
     --nnodes=1 \
     --nproc_per_node=8 \
-    train.py
+    train.py \
+    > shell.log
 # The ModelArguments, DataArguments, TrainingArguments are all embedded in the train.py file.
